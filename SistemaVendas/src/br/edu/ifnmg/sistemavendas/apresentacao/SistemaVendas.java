ff1eb783fc42916f8c034f6dfbffe431125952ac
JUCIELIO <JUCIELIO@JUCIELIO-PC>
2013-05-08 23:40:04 -0300

    Teste da classe Sistrema Vendas

---
+++ SistemaVendas/src/br/edu/ifnmg/sistemavendas/apresentacao/SistemaVendas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.sistemavendas.apresentacao;
+
+import java.util.Date;
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import org.eclipse.persistence.jpa.internal.jpql.parser.DateTime;
+
+/**
+ *
+ * @author JUCIELIO
+ */
+public class SistemaVendas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      System.out.println("Bem-vindo ao Sistema de Vendas  ");
+        
+    }
+}
